143abbfa73f028e83e521918f617bb40b8c1ede7
t0e <thetmgmgtoe7@gmail.com>
2026-02-16 17:21:08 +0700

    add global error handler

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,12 @@
+// 🔥 Temporary global error handler (debug only)
+window.onerror = function (message, _source, lineno, colno) {
+    alert(`JS Error:\n${message}\nLine: ${lineno}\nColumn: ${colno}`)
+}
+
+window.onunhandledrejection = function (event) {
+    alert('Unhandled Promise Error:\n' + event.reason)
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
