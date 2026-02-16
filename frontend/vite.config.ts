0f0d312b90333f189256a065e07e26613fc1a568
t0e <thetmgmgtoe7@gmail.com>
2026-02-16 16:54:39 +0700

    add legacy plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [
+        react(),
+        legacy({
+            targets: ['defaults', 'not IE 11']
+        }),
+        tailwindcss()
+    ]
 })
